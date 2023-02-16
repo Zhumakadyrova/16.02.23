d9170736d1e74b090b72c0524601cfa86cfca8a0
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-02-16 14:20:49 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// Много уровневый массив
+let example1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// matrix
+let example2 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];   
+
+console.log(example1.length);
+console.log(example2.length);
+
+console.log(example1[2]);
+console.log(example2[2]);
